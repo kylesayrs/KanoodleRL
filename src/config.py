2558a182aadb67d0570ce8185c4737c5c205b5ac
Kyle Sayers <kylesayrs@gmail.com>
2023-06-29 14:48:38 -0400

    increased net_arch -> finally learning!

--- src/config.py
+++ src/config.py
@@ -3,19 +3,18 @@ from pydantic import BaseModel, Field
 
 import torch
 from stable_baselines3.common.buffers import ReplayBuffer
-from stable_baselines3.common.noise import NormalActionNoise
 
 from src.utils import Immutable
 
 
 class TrainingConfig(Immutable, BaseModel):
     n_envs: int = Field(default=2)
-    total_timesteps: float = Field(default=200_000)  # demo: 15k
+    total_timesteps: float = Field(default=30_000)  # demo: 15k
 
-    model_arch: str = "DQN"
+    model_arch: str = "DDPG"
 
-    wandb_mode: str = Field(default="disabled")
-    log_interval: int = Field(default=1000)
+    wandb_mode: str = Field(default="online")
+    log_interval: int = Field(default=10)
     n_eval_episodes: int = Field(default=0)
     eval_freq: int = Field(default=1_000, description="num steps")
     eval_render: bool = Field(default=False)
@@ -26,7 +25,8 @@ class TrainingConfig(Immutable, BaseModel):
 class ModelConfig(Immutable, BaseModel):
     policy: str = Field(default="MultiInputPolicy")
     policy_kwargs: str = Field(default={
-        "activation_fn": torch.nn.ReLU
+        "activation_fn": torch.nn.ReLU,
+        "net_arch": [128, 64, 32]
     })
 
     verbose: int = Field(default=2)
@@ -37,26 +37,9 @@ class ModelConfig(Immutable, BaseModel):
         arbitrary_types_allowed = True
 
 
-class DDPGConfig(ModelConfig):
-    learning_starts: int = Field(default=128)
-    learning_rate: float = Field(default=1e-6)  # demo: 1e-5
-    train_freq: Tuple[int, str] = Field(default=(10, "step"))
-    batch_size: int = Field(default=128)
-    gamma: float = Field(default=0.90)
-
-    action_noise: Optional[str] = Field(default="Normal")
-    action_noise_mu: float = Field(default=0.0)
-    action_noise_sigma: float = Field(default=0.15)
-
-    buffer_size: int = Field(default=100_000)
-    optimize_memory_usage: bool = Field(default=False)
-    replay_buffer_class: Optional[ReplayBuffer] = Field(default=None)
-    replay_buffer_kwargs: Optional[Dict[str, Any]] = Field(default=None)
-
-
 class DQNConfig(ModelConfig):
     learning_starts: int = Field(default=128)
-    learning_rate: float = Field(default=1e-5)  # demo: 1e-4
+    learning_rate: float = Field(default=1e-2)  # demo: 1e-2
     train_freq: Tuple[int, str] = Field(default=(10, "step"))
     batch_size: int = Field(default=128)
     gamma: float = Field(default=0.99)
@@ -65,17 +48,32 @@ class DQNConfig(ModelConfig):
     exploration_initial_eps: float = Field(default=1.0)
     exploration_final_eps: float = Field(default=0.05)
 
-    buffer_size: int = Field(default=100_000)
+    buffer_size: int = Field(default=50_000)
     optimize_memory_usage: bool = Field(default=False)
-    replay_buffer_class: Optional[ReplayBuffer] = Field(default=None)
-    replay_buffer_kwargs: Optional[Dict[str, Any]] = Field(default=None)
+
+
+class DDPGConfig(ModelConfig):
+    learning_starts: int = Field(default=128)
+    learning_rate: float = Field(default=1e-6)  # demo: 1e-5
+    train_freq: Tuple[int, str] = Field(default=(10, "step"))
+    batch_size: int = Field(default=128)
+    gamma: float = Field(default=0.99)
+
+    action_noise: Optional[str] = Field(default="Normal")
+    action_noise_mu: float = Field(default=0.0)
+    action_noise_sigma: float = Field(default=0.02)
+
+    buffer_size: int = Field(default=50_000)
+    optimize_memory_usage: bool = Field(default=False)
 
 
 class PPOConfig(ModelConfig):
-    learning_rate: float = Field(default=7e-6)
+    learning_rate: float = Field(default=3e-7)
     n_steps: float = Field(default=32, description="The number of steps to run for each environment per update")
     batch_size: int = Field(default=8)
     n_epochs: int = Field(default=5)
+
+    ent_coef: float = Field(default=0.1)
 
     gamma: float = Field(default=0.99)
     gae_lambda: float = Field(default=0.95)
@@ -84,12 +82,12 @@ class PPOConfig(ModelConfig):
 
 
 class EnvironmentConfig(BaseModel):
-    board_shape: Tuple[int, int] = Field(default=(3, 3))
-    pieces_set_name: str = Field(default="demo")
+    board_shape: Tuple[int, int] = Field(default=(5, 5))
+    pieces_set_name: str = Field(default="junior")
 
-    discrete: bool = Field(default=True)
-    prevent_invalid_actions: bool = Field(default=False)
-    calc_unsolvable: bool = Field(default=False)
+    discrete: bool = Field(default=False)
+    prevent_invalid_actions: bool = Field(default=True)
+    calc_unsolvable: bool = Field(default=True)
 
     complete_reward: float = Field(default=10.0)
     fail_reward: float = Field(default=-10.0)
